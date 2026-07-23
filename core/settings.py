8b73c8967aa478f6c37252a8788d4ce127db1638
josemarsalvador7 <josemarsalvador2027@gmail.com>
2026-07-23 16:09:34 +0100

    adicionado um swaggwe

--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,7 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 
-EXTERNAL_APPS = ["rest_framework"]
+EXTERNAL_APPS = ["rest_framework",'drf_spectacular',]
 
 MY_APPS = [
     "skills.apps.SkillsConfig",
@@ -124,3 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Simple Jobs API',
+    'DESCRIPTION': 'Jobs API',
+    'VERSION': '1.0.0',
+}
--- core/urls.py
+++ core/urls.py
@@ -17,9 +17,13 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/skills/", include("skills.urls")),
-    path("api/jobs/", include("jobs.urls")),
+    path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/v1/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/v1/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path("api/v1/skills/", include("skills.urls")),
+    path("api/v1/jobs/", include("jobs.urls")),
+    path("", admin.site.urls),
 ]
